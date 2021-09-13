b0e1421186fbe2f0e33211101fccbf74fb82f0f3
Adriano Oliveira Castro <56028189+aocastro@users.noreply.github.com>
2021-09-13 13:46:31 +0000

    Aula do dia 13/09/2021

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
+
     $('#btn-menu').click(function(e){
         e.preventDefault()
         $('#menu').toggle('slow')
+    })
+
+    // Pegando os link do menu para apresentar a página
+    $('a').click(function(e){
+        e.preventDefault()
+        // Variável para se guardar qual link abrir
+        let url = $(this).attr('href')
+        $('#'+url).show()
     })
 })
